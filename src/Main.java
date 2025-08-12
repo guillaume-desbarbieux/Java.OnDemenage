def645b5e8b9f65ab7b78493f9bd313c462f9cdd
guillaume-desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-08-12 14:14:22 +0200

    Un seul println (hormis la saisie utilisateur)

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,34 @@ public class Main {
 
         Scanner clavier = new Scanner(System.in);
 
-        int totalCartons;
-        int capaciteCamion;
+        int totalCartons = 0;
+        int capaciteCamion = 0;
 
         do {
-            System.out.println("Quel est le nombre total de cartons ?");
-            totalCartons = clavier.nextInt();
-            clavier.nextLine();
+            if (totalCartons < 1) {
+                System.out.println("Quel est le nombre total de cartons ?");
+                totalCartons = clavier.nextInt();
+                clavier.nextLine();
+            }
 
-            System.out.println("Quel est la capacité de chargement du camion ?");
-            capaciteCamion = clavier.nextInt();
-            clavier.nextLine();
-
-        } while (totalCartons<1 || capaciteCamion<1);
+            if (capaciteCamion < 1) {
+                System.out.println("Quel est la capacité de chargement du camion ?");
+                capaciteCamion = clavier.nextInt();
+                clavier.nextLine();
+            }
+        } while (totalCartons < 1 || capaciteCamion < 1);
 
         clavier.close();
 
-        int nbVoyagesPleins = totalCartons/capaciteCamion;
-        int cartonsRestants = totalCartons%capaciteCamion;
-
-        System.out.println(("Un voyage de " + capaciteCamion + " cartons.\n").repeat(nbVoyagesPleins));
-
-        System.out.println("Nous avons fait " + nbVoyagesPleins + " voyages de " + capaciteCamion + " cartons.");
-
-        if (cartonsRestants > 0) {
-            System.out.println("Faisons un dernier voyage de " + cartonsRestants + " cartons.");
+        int nbVoyagesPleins = totalCartons / capaciteCamion;
+        int cartonsRestants = totalCartons % capaciteCamion;
+        int dernierVoyage = 1;
+        if (cartonsRestants == 0) {
+            dernierVoyage = 0;
         }
+
+        System.out.println(
+                 ("Un voyage de " + capaciteCamion + " cartons.\n").repeat(nbVoyagesPleins)
+                +("Un dernier voyage de " + cartonsRestants + " cartons.").repeat(dernierVoyage));
     }
 }
