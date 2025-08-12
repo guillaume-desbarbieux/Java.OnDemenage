95c60fb1a4bf728828486e89ea99ee32235a521c
guillaume-desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-08-12 12:47:23 +0200

    Ajout vérification des entrées et message de résumé

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,36 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner clavier = new Scanner(System.in);
-        System.out.println("Quel est le nombre total de cartons ?");
-        int totalCartons = clavier.nextInt();
 
-        System.out.println("Quel est la capacité de chargement du camion ?");
-        int capaciteCamion = clavier.nextInt();
+        Scanner clavier = new Scanner(System.in);
+
+        int totalCartons = 0;
+        while(totalCartons<1) {
+            System.out.println("Quel est le nombre total de cartons ?");
+            totalCartons = clavier.nextInt();
+            clavier.nextLine();
+        }
+
+        int capaciteCamion = 0;
+        while(capaciteCamion<1) {
+            System.out.println("Quel est la capacité de chargement du camion ?");
+            capaciteCamion = clavier.nextInt();
+            clavier.nextLine();
+        }
 
         clavier.close();
 
         int cartonsRestants = totalCartons;
+        int nombreDeVoyages = 0;
 
-        while (cartonsRestants > capaciteCamion) {
+        while (cartonsRestants >= capaciteCamion) {
+            nombreDeVoyages++;
             System.out.println("Un voyage de " + capaciteCamion + " cartons.");
             cartonsRestants -= capaciteCamion;
         }
+        System.out.println("Nous avons fait " + nombreDeVoyages + " voyages de " + capaciteCamion + " cartons.");
         if (cartonsRestants > 0) {
-            System.out.println("Un dernier voyage de " + cartonsRestants + " cartons.");
+            System.out.println("Faisons un dernier voyage de " + cartonsRestants + " cartons.");
         }
     }
 }
