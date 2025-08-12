53c06255fab78b1f2b2b0c45c8f3358ad18ef722
guillaume-desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-08-12 14:59:15 +0200

    Création javadoc

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
         int totalCartons = lireEntierPositifClavier("Quel est le nombre total de cartons ?");
         int capaciteCamion = lireEntierPositifClavier("Quelle est la capacité de chargement du camion ?");
-
         int nbVoyagesPleins = totalCartons / capaciteCamion;
         int cartonsRestants = totalCartons % capaciteCamion;
         int dernierVoyage = (cartonsRestants > 0) ? 1 : 0;
-
         afficherMessage(("Un voyage de " + capaciteCamion + " cartons.\n").repeat(nbVoyagesPleins));
         afficherMessage(("Un dernier voyage de " + cartonsRestants + " cartons.").repeat(dernierVoyage));
     }
 
+    /**
+     * Lis une entrée clavier
+     * N'accepte que les entiers strictement positif
+     * @param message Le message affiché à l'utilisateur pour le guider dans sa saisie
+     * @return l'entier strictement positif saisi par l'utilisateur
+     */
     public static int lireEntierPositifClavier(String message){
         Scanner clavier = new Scanner(System.in);
         int valeur;
@@ -27,6 +28,10 @@ public class Main {
         return valeur;
     }
 
+    /**
+     * Affiche un message dans le terminal
+     * @param message Le message à afficher
+     */
     public static void afficherMessage(String message){
         System.out.println(message);
     }
