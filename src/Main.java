ddda5f22169f42fe931db185bec4ce841c779a43
guillaume-desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-08-13 16:32:07 +0200

    Ecriture dans un fichier

--- src/Main.java
+++ src/Main.java
@@ -9,48 +9,89 @@ public class Main {
         int nbVoyagesPleins = totalCartons / capaciteCamion;
         int cartonsRestants = totalCartons % capaciteCamion;
         int dernierVoyage = (cartonsRestants > 0) ? 1 : 0;
-        writeToFile(("Un voyage de " + capaciteCamion + " cartons.\n").repeat(nbVoyagesPleins));
-        writeToFile(("Un dernier voyage de " + cartonsRestants + " cartons.").repeat(dernierVoyage));
+
+        FileWriter writer = openFileWriter("output.txt", false);
+        if (writer != null) {
+            writeToFile(writer, ("Un voyage de " + capaciteCamion + " cartons.\n").repeat(nbVoyagesPleins));
+            writeToFile(writer, ("Un dernier voyage de " + cartonsRestants + " cartons.").repeat(dernierVoyage));
+            closeFileWriter(writer);
+        } else {
+            System.out.println("Erreur !");
+        }
+
+
     }
 
     /**
      * Lis une entrée clavier
      * N'accepte que les entiers strictement positifs
+     *
      * @param message Le message affiché à l'utilisateur pour le guider dans sa saisie
      * @return l'entier strictement positif saisi par l'utilisateur
      */
-    public static int lireEntierPositifClavier(String message){
+    public static int lireEntierPositifClavier(String message) {
         Scanner clavier = new Scanner(System.in);
         int valeur;
         try {
             do {
-                afficherMessage(message);
+                displayMessage(message);
                 valeur = clavier.nextInt();
                 clavier.nextLine();
             } while (valeur < 1);
             return valeur;
         } catch (Exception e) {
-            afficherMessage("Erreur de saisie. \n" + e.getMessage());
+            displayMessage("Erreur de saisie. \n" + e.getMessage());
         }
         return 0;
     }
 
     /**
      * Affiche un message dans le terminal
+     *
+     *
      * @param message Le message à afficher
      */
-    public static void afficherMessage(String message){
+    public static void displayMessage(String message) {
         System.out.println(message);
     }
 
-    public static void writeToFile(String message) {
+    /**
+     * Ouvre un FileWriter
+     * @param filename Nom du fichier de destination
+     * @param append Booléen (Vrai pour écrire à la suite, Faux pour écraser le fichier)
+     * @return le FileWriter
+     */
+    public static FileWriter openFileWriter(String filename, Boolean append){
         try {
-            FileWriter writer = new FileWriter("output.txt", true);
-            writer.write(message);
-            writer.close();
-
+            return new FileWriter(filename, append);
         } catch (IOException e) {
-            System.out.println("An error occurred.");
+            System.out.println("An opening error occurred.");
+        }
+        return null;
+    }
+
+    /**
+     * Ecris un message dans un fichier
+     * @param writer Writer à utiliser
+     * @param message Message à écrire
+     */
+    public static void writeToFile(FileWriter writer, String message) {
+        try {
+            writer.write(message);
+        } catch (IOException e) {
+            System.out.println("A writing error occurred.");
+        }
+    }
+
+    /**
+     * Ferme un FileWriter
+     * @param writer writer à fermer
+     */
+    public static void closeFileWriter(FileWriter writer) {
+        try {
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("A closing error occurred.");
         }
     }
 }
