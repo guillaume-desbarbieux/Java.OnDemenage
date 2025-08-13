7b6e9af66747059c811b25a22bc9d42c50bf83a1
guillaume-desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-08-13 15:44:50 +0200

    Ecriture dans un fichier

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,25 +9,30 @@ public class Main {
         int nbVoyagesPleins = totalCartons / capaciteCamion;
         int cartonsRestants = totalCartons % capaciteCamion;
         int dernierVoyage = (cartonsRestants > 0) ? 1 : 0;
-        afficherMessage(("Un voyage de " + capaciteCamion + " cartons.\n").repeat(nbVoyagesPleins));
-        afficherMessage(("Un dernier voyage de " + cartonsRestants + " cartons.").repeat(dernierVoyage));
+        writeToFile(("Un voyage de " + capaciteCamion + " cartons.\n").repeat(nbVoyagesPleins));
+        writeToFile(("Un dernier voyage de " + cartonsRestants + " cartons.").repeat(dernierVoyage));
     }
 
     /**
      * Lis une entrée clavier
-     * N'accepte que les entiers strictement positif
+     * N'accepte que les entiers strictement positifs
      * @param message Le message affiché à l'utilisateur pour le guider dans sa saisie
      * @return l'entier strictement positif saisi par l'utilisateur
      */
     public static int lireEntierPositifClavier(String message){
         Scanner clavier = new Scanner(System.in);
         int valeur;
-        do {
-            afficherMessage(message);
-            valeur = clavier.nextInt();
-            clavier.nextLine();
-        } while (valeur < 1);
-        return valeur;
+        try {
+            do {
+                afficherMessage(message);
+                valeur = clavier.nextInt();
+                clavier.nextLine();
+            } while (valeur < 1);
+            return valeur;
+        } catch (Exception e) {
+            afficherMessage("Erreur de saisie. \n" + e.getMessage());
+        }
+        return 0;
     }
 
     /**
@@ -34,5 +41,17 @@ public class Main {
      */
     public static void afficherMessage(String message){
         System.out.println(message);
+    }
+
+    public static void writeToFile(String message) {
+        try {
+            FileWriter writer = new FileWriter("output.txt", true);
+            writer.write(message);
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
